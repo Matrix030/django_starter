4781f97a520f690218e4a6032093adbb0f227ce0
Matrix030 <rishigharat@gmail.com>
2025-09-28 11:40:18 -0400

    fixed model

--- polls/models.py
+++ polls/models.py
@@ -2,14 +2,20 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-class Questions(models.Model):  # Singular
+class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField('date published')
     
-    def __str__(self) -> str:
+    def __str__(self):
         return self.question_text
+        
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)  # Singular
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,15 @@
 from django.http import HttpResponse, Http404
-from .models import Questions
+from .models import Question
 from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    latest_question_list = Questions.objects.order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Questions, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
